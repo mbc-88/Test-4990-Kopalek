b808c5d52defe64f072d798471c7a32b02b6b54a
mbc-88 <marinab.c.88@gmail.com>
2025-04-24 21:08:49 -0400

    change base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/4990-Final-Project",          // same as GitHub Pages sub-path
+  base: "/Test-4990-Kopalek",          // same as GitHub Pages sub-path
   plugins: [react()],
 })
